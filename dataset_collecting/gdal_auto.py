7f3e9a12322fe811a5b1f34371bd16694d610b36
dominhnhut01 <62295931+dominhnhut01@users.noreply.github.com>
2020-08-14 21:36:33 +0700

    Fix Tsoi's error

--- dataset_collecting/gdal_auto.py
+++ dataset_collecting/gdal_auto.py
@@ -1,21 +1,17 @@
 import os
-import numpy as np
-
 
 #direc = "./input"
 #out_direc = "~/Yours/Projects/SDC/dataset/output"
 direc = input("Enter input data directory here: ")
 out_direc = input("Enter output data directory here: ")
 num = 1
-for fil in os.listdir(direc):
-    name = fil[4:-4]
+for file in os.listdir(direc):
+    name = file[4:-4]
     print(name)
-    os.system("gdal_translate -of JPEG -scale -co worldfile=yes {}/{} {}/{}.jpg".format(direc,fil,out_direc,name))
-    num = num+1
+    print("gdal_translate -of JPEG -scale -co worldfile=yes {}/{} {}/{}.jpg".format(direc,file,out_direc,name))
+    os.system("gdal_translate -of JPEG -scale -co worldfile=yes {}/{} {}/{}.jpg".format(direc,file,out_direc,name))
+    num+=1
 
-num_del = 1
-for fil in os.listdir("{}".format(out_direc)):
-    if (fil[-3:]!="jpg"):
-        os.system("rm -r {}/{}".format(out_direc,fil))
-
-
+for file in os.listdir(out_direc):
+    if (file[-3:]!="jpg"):
+        os.system("rm -r {}/{}".format(out_direc,file))
