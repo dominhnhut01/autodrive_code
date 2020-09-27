3686b460cfda0f389a3ae917d1c742b11239fad2
dominhnhut01 <dominhnhut01@gmail.com>
2020-09-27 14:50:16 +0700

    Minor adjustment

--- dataset_collecting/annotation/elimination.py
+++ dataset_collecting/annotation/elimination.py
@@ -1,24 +1,30 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-folder = "/home/tsoi/Yours/Projects/SDC/data/fixed_array"
-out_folder = "/home/tsoi/Yours/Projects/SDC/data/not_eliminated_array"
+#in_direc = "/home/tsoi/Yours/Projects/SDC/data/fixed_array1"
+#out_direc = "/home/tsoi/Yours/Projects/SDC/data/fixed_array2"
+in_direc = input("Enter input data directory here: ")
+out_direc = input("Enter output data directory here: ")
+
 percentage = 0.8
 threshold = 100
 points = []
-for file in os.listdir(folder):
+
+for file in os.listdir(in_direc):
     try:
-        imap = np.load(folder+"/"+file)
+        imap = np.load(in_direc+"/"+file)
         registered_point = np.count_nonzero(imap == 255)
         ratio = registered_point/imap.size
-        if (ratio>=0.2 and ratio<=0.6):
-            np.save(out_folder+"/"+file,imap)
+        #points.append(ratio)
+        if (ratio>0.01 and ratio<=0.7):
+            np.save(out_direc+"/"+file,imap)
     except:
         continue
-
-#x = points
-#x.sort()
-#range(len(x))
-#range(len(points))
-#plt.scatter(range(len(points)),x)
-#plt.show()
+'''
+x = points
+x.sort()
+range(len(x))
+range(len(points))
+plt.scatter(range(len(points)),x)
+plt.show()
+'''
