c9924433ba0693ac77fdf516fd185701fc6c4197
dominhnhut01 <dominhnhut01@gmail.com>
2020-10-03 14:14:28 +0700

    Add code comments, adjust ambigous variable names, and add cross-OS directory code

--- dataset_collecting/annotation/elimination.py
+++ dataset_collecting/annotation/elimination.py
@@ -1,30 +1,40 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-#in_direc = "/home/tsoi/Yours/Projects/SDC/data/fixed_array1"
-#out_direc = "/home/tsoi/Yours/Projects/SDC/data/fixed_array2"
-in_direc = input("Enter input data directory here: ")
-out_direc = input("Enter output data directory here: ")
 
-percentage = 0.8
-threshold = 100
-points = []
+def del_invalid(in_dir,out_dir,points):
+	#This function is used to delete possible invalid annotation masks, which are almost black or almost white
+	for file in os.listdir(in_dir):
+	    try:
+	        imap = np.load(in_dir+"/"+file)
+	        registered_point = np.count_nonzero(imap == 255)
+	        ratio = registered_point/imap.size
+	        points.append(ratio)
+	        if (ratio>0.01 and ratio<=0.7):
+	            np.save(out_dir+"/"+file,imap)
+	    except:
+	        continue
+def visualize(points):
+	#This function is used to visualize the initial pixel distribution in the dataset
+	x = points
+	x.sort()
+	range(len(x))
+	range(len(points))
+	plt.scatter(range(len(points)),x)
+	plt.show()
 
-for file in os.listdir(in_direc):
-    try:
-        imap = np.load(in_direc+"/"+file)
-        registered_point = np.count_nonzero(imap == 255)
-        ratio = registered_point/imap.size
-        #points.append(ratio)
-        if (ratio>0.01 and ratio<=0.7):
-            np.save(out_direc+"/"+file,imap)
-    except:
-        continue
-'''
-x = points
-x.sort()
-range(len(x))
-range(len(points))
-plt.scatter(range(len(points)),x)
-plt.show()
-'''
+if __name__ == '__main__':
+	
+	points = []
+
+	#Get the current directory
+	os.chdir('../../../dataset/')
+	main_dir = os.getcwd()
+	in_folder = input("Enter input folder name here: ")
+	out_folder = input("Enter output folder name here: ")
+	
+	#Create absolute path:
+	in_dir = os.path.join(main_dir,in_folder)
+	out_dir = os.path.join(main_dir,out_folder)
+	del_invalid(in_dir,out_dir,points)
+	visualize(points) 
--- dataset_collecting/annotation/road_annotation.py
+++ dataset_collecting/annotation/road_annotation.py
@@ -14,6 +14,7 @@ imap = np.array([[0,0,1,0,2,0,1,0,0],
                  [1,1,0,0,2,1,0,0,0],
                  [0,1,0,0,2,1,0,0,0]])
 '''
+
 def check_validity(x,y):
     if (x<=-1 or y<=-1 or x>=length or y>=width):
         return 0
@@ -23,6 +24,12 @@ def check_validity(x,y):
         return 1
 
 def flood_fill(x,y,color):
+    '''
+    This function is used to mark separated areas with different value (color)
+    Param:
+    x,y: coordinate of the start point
+    color: initial color for the first area
+    '''
     point_queue = []
     point_queue.append((x,y))
     while (len(point_queue)!=0):
@@ -68,41 +75,53 @@ def spr2(x,y):
             container.append(imap[y][x])
         return
 
-#data_input = input("Input your input folder directory: ")
-#data_output = input("Input your output folder directory: ")
+def main(in_dir,out_dir):
+    '''
+    This function is used to export the output annotation mask npy file
+    Param:
+    - in_dir: Input folder directory
+    - out_dir: Output folder directory
+    '''
+    for file in in_dir:
+        name = file[:-4]
+        imap = np.load(data_input+"/"+file)
+        length = imap.shape[1]
+        width = imap.shape[0]
+        check = np.zeros((width,length))
+        color = 3
+        container = []
+        for y in range(width):
+            for x in range(length):
+                if (imap[y][x]==0):
+                    flood_fill(x,y,color)
+                    color= color+1
 
-data_input = "/home/tsoi/Yours/Projects/SDC/data/array1"
-data_output = "/home/tsoi/Yours/Projects/SDC/data/fixed_array1"
+        for y in range(width):
+            for x in range(length):
+                if (imap[y][x]==2):
+                    spr2(x,y+1)
+                    spr2(x+1,y)
+                    spr2(x-1,y)
+                    spr2(x,y-1)
+        for y in range(width):
+            for x in range(length):
+                if (imap[y][x] in container or imap[y][x]==2):
+                    imap[y][x]=255
+                else:
+                    imap[y][x]=0
 
-file_direc = os.listdir(data_input)
+        np.save(out_dir+"/"+name+".npy",imap)
 
 
-for file in file_direc:
-    name = file[:-4]
-    imap = np.load(data_input+"/"+file)
-    length = imap.shape[1]
-    width = imap.shape[0]
-    check = np.zeros((width,length))
-    color = 3
-    container = []
-    for y in range(width):
-        for x in range(length):
-            if (imap[y][x]==0):
-                flood_fill(x,y,color)
-                color= color+1
-
-    for y in range(width):
-        for x in range(length):
-            if (imap[y][x]==2):
-                spr2(x,y+1)
-                spr2(x+1,y)
-                spr2(x-1,y)
-                spr2(x,y-1)
-    for y in range(width):
-        for x in range(length):
-            if (imap[y][x] in container or imap[y][x]==2):
-                imap[y][x]=255
-            else:
-                imap[y][x]=0
-
-    np.save(data_output+"/"+name+".npy",imap)
+if __name__ == '__main__':
+    
+    #Get the current directory
+    os.chdir('../../../dataset/')
+    main_dir = os.getcwd()
+    in_folder = input("Enter input folder name here: ")
+    out_folder = input("Enter output folder name here: ")
+    
+    #Create absolute path:
+    in_dir = os.path.join(main_dir,in_folder)
+    out_dir = os.path.join(main_dir,out_folder)
+    main(in_dir,out_dir)
