38edad5c56de3b0bb281ffdac3321f5e1aca8300
OrCourt <pntsoi@ucsd.edu>
2020-10-18 13:23:41 +0700

    modify tfrecords to data

--- dataset_collecting/Dataset_convert/tfrecords_to_data.py
+++ dataset_collecting/Dataset_convert/tfrecords_to_data.py
@@ -1,42 +1,66 @@
 import numpy as np
 import tensorflow as tf
 import os
-import IPython.display as display
 import PIL
+'''
+This code takes in jpg images (since tf can't read tiff) to save it as a feature in TFrecord
+so you should run tif_to_jpg.py first.
+'''
 
-def read_record(in_dir):
-    # Read the TFRecord file
-    raw_data = tf.data.TFRecordDataset(in_dir)
-    feature_description = {
-        'height': tf.io.FixedLenFeature([1],tf.int64),
-        'width': tf.io.FixedLenFeature([1],tf.int64),
-        'depth': tf.io.FixedLenFeature([1],tf.int64),
-        'label': tf.io.FixedLenFeature([1],tf.int64),
-        'image': tf.io.FixedLenFeature((),tf.string),
-        'numpy': tf.io.FixedLenFeature([873,441],tf.int64)
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+in_direc_jpg = "/home/tsoi/Yours/Projects/SDC/data/main_dataset/satellite_jpg"
+in_direc_npy = "/home/tsoi/Yours/Projects/SDC/data/main_dataset/street_annotate_npy"
+out_direc = "/home/tsoi/Yours/Projects/SDC/data/main_dataset/tfrecords"
+
+def write_record(label,img_string,npy,out_direc,width,height, tfrecord_name):
+    feature = {
+        'height': _int64_feature([height]),
+        'width': _int64_feature([width]),
+        'depth': _int64_feature([1]),
+        'label': _int64_feature([int(label)]),
+        'image': _bytes_feature(img_string),
+        'numpy': _int64_feature(npy)
     }
-    def _parse_image_function(example_proto):
-        # Parse the input `tf.train.Example` proto using the dictionary above.
-        return tf.io.parse_single_example(example_proto, feature_description)
-    parsed_image_dataset = raw_data.map(_parse_image_function)
-    #print(parsed_image_dataset)
-    for image_features in parsed_image_dataset:
-        image_raw = image_features['image'].numpy()
-        display.display(display.Image(data=image_raw))
-        #print(image_raw)
-'''
-Test code:
-for image_features in parsed_image_dataset:
-    image_raw = image_features['image_raw'].numpy()
-    display.display(display.Image(data=image_raw))
-'''
-if __name__ == '__main__':
-    
-    #Get the current directory
-    os.chdir('../../../dataset/')
-    main_dir = os.getcwd()
-    in_file = input("Enter input TFRecord file name here: ")
-    
-    #Create absolute path:
-    in_dir = os.path.join(main_dir,in_file)
-    read_record(in_dir)
+    tf_example = tf.train.Example(features=tf.train.Features(feature=feature))
+    with tf.io.TFRecordWriter(out_direc+"/"+'data'+str(tfrecord_name)+'.tfrecords') as writer:
+        writer.write(tf_example.SerializeToString())
+
+def serialize_array(array):
+  array = tf.io.serialize_tensor(array)
+  return array
+
+def tfrecord_write_from_folder(in_direc_jpg,in_direc_npy,out_direc):
+    JPGs = os.listdir(in_direc_jpg)
+    NPYs = os.listdir(in_direc_npy)
+    file_number = 0
+    tfrecord_name = 0
+    for jpg in JPGs:
+        file_number = file_number + 1
+        if (file_number >= 1000):
+            tfrecord_name = tfrecord_name + 1
+            file_number = 0
+        jpg_direc = in_direc_jpg+"/"+jpg
+        img_string = open(jpg_direc,'rb').read()
+        image = PIL.Image.open(jpg_direc)
+        width, height = image.size
+        name = jpg[4:-4]
+        numpy_data = np.load(in_direc_npy+"/"+"street_data_"+name+".npy")
+        numpy_data= numpy_data.reshape((1,width*height))
+        npy = numpy_data[0]
+        #npy = numpy_data.tolist()
+        write_record(name,img_string,npy,out_direc,width,height,tfrecord_name)
+
+tfrecord_write_from_folder(in_direc_jpg,in_direc_npy,out_direc)
