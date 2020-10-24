637e15c936c95a38d610897619efe2713bed566e
dominhnhut01 <dominhnhut01@gmail.com>
2020-10-24 12:06:01 +0700

    adjust jpg to png because jpg format loses data'

--- dataset_collecting/Dataset_convert/npy_to_png.py
+++ dataset_collecting/Dataset_convert/npy_to_png.py
@@ -2,18 +2,18 @@ import numpy as np
 from PIL import Image
 import os
 
-def npy_to_jpg(in_dir,out_dir):
+def npy_to_png(in_dir,out_dir):
 	'''
-	This function would convert all the .npy files in the input folder to .jpg file, which would be put in the output folder
+	This function would convert all the .npy files in the input folder to .png file, which would be put in the output folder
 	Param:
 	- in_dir: The directory of the input folder (folder of .npy files)
-	- out_dir: The directory of the output folder (folder of .jpg files)
+	- out_dir: The directory of the output folder (folder of .png files)
 	Return: This function return nothing
 	'''	
 	for file in os.listdir(in_dir):
         im = np.load(in_dir+"/" + file)
         img = Image.fromarray(im)
-	    img.save("{}/{}.jpg".format(out_dir,file[:-4]))
+	    img.save("{}/{}.png".format(out_dir,file[:-4]))
 
 if __name__ == '__main__':
 	#Get the current directory
@@ -25,4 +25,4 @@ if __name__ == '__main__':
 	#Create absolute path:
 	in_dir = os.path.join(main_dir,in_folder)
 	out_dir = os.path.join(main_dir,out_folder)
-	npy_to_jpg(in_dir,out_dir)
+	npy_to_png(in_dir,out_dir)
